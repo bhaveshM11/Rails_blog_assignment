2c9357cab9bfb80302f8b9ff00adc6a86d10eae7
Bhavesh Mandle <bhavesh.mandle@coupa.com>
2025-08-15 16:44:08 +0530

    List and create, edit for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,33 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    status = @article.save
+    if status
+      flash[:notice] = "Article created successfully."
+      redirect_to @article
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article updated successfully."
+      redirect_to @article
+    else
+      render action: "edit"
+    end
+  end
 end
